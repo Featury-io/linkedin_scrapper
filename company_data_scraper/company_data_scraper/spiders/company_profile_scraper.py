4a3c586d25211a3bd35118513e039a5685159a90
Othmane Bouziane <othmane@featury.io>
2024-11-04 15:06:25 +0100

    Fix redirect

--- company_data_scraper/company_data_scraper/spiders/company_profile_scraper.py
+++ company_data_scraper/company_data_scraper/spiders/company_profile_scraper.py
@@ -4,19 +4,18 @@ import scrapy
 from scrapy.http import Request, Response
 import re
 
-input_file = 'directorydata.json'
 input_file = 'company_names.json'
-desired_company_names = ["OpenAI", "Microsoft"] # Please make sure to check the spellings of the names given
 company_urls = []
 
 def get_url_by_company_name():
+    global company_urls
     try:
         with open(input_file, 'r') as json_file:
             data = json.load(json_file)
 
             for v in data.values():
                 company_urls.append(v+"/?trk=companies_directory")
-            
+            company_urls = company_urls[:500]
             print("Company URLs:", len(company_urls))
     except FileNotFoundError:
         print(f"Error: JSON file '{input_file}' not found.")
@@ -28,7 +27,9 @@ class CompanyProfileScraperSpider(scrapy.Spider):
     name = 'company_profile_scraper'
     
     custom_settings = {
-        'HTTPERROR_ALLOW_ALL': True,  # Allows handling all HTTP status codes
+        'HTTPERROR_ALLOW_ALL': True, 
+        'REDIRECT_ENABLED': True, 
+        'REDIRECT_MAX_TIMES': 5, 
     }
     
     def __init__(self, *args, **kwargs):
@@ -51,6 +52,16 @@ class CompanyProfileScraperSpider(scrapy.Spider):
 
     def parse_response(self, response):
         company_index_tracker = response.meta['company_index_tracker']
+        
+        # Check for redirect responses
+        if response.status in [301, 302, 303, 307, 308]:
+            print(f"Redirect encountered for {response.url}. Handling redirect.")
+            yield scrapy.Request(url=response.headers['Location'].decode('utf-8'), 
+                                 callback=self.parse_response, 
+                                 meta={'company_index_tracker': company_index_tracker},
+                                 dont_filter=True)
+            return
+        
         if response.status == 404:
             print(f"Skipping {str(company_index_tracker + 1)} of {str(len(self.company_pages))}")
         else:
@@ -90,62 +101,54 @@ class CompanyProfileScraperSpider(scrapy.Spider):
                 print("Error occurred while getting number of employees: {e}")
 
             try:
-                company_details = response.css(
-                    '.core-section-container__content .mb-2')
+                company_details = response.css('.core-section-container__content .mb-2')
 
-                company_item['website'] = company_details[0].css(
-                    'a::text').get(default='not-found').strip()
-
-                company_industry_line = company_details[1].css(
-                    '.text-md::text').getall()
-                company_item['industry'] = company_industry_line[1].strip()
-
-                company_size_line = company_details[2].css(
-                    '.text-md::text').getall()
-                company_item['company_size_approx'] = company_size_line[1].strip().split()[
-                    0]
-
-                company_headquarters = company_details[3].css(
-                    '.text-md::text').getall()
-                if company_headquarters[0].lower().strip() == 'headquarters':
-                    company_item['headquarters'] = company_headquarters[1].strip()
-                else:
-                    company_item['headquarters'] = 'not-found'
-
-                company_type = company_details[4].css('.text-md::text').getall()
-                company_item['type'] = company_type[1].strip()
-
-                # specialities or founded, one among them -> storing in `unsure_parameter`
-                unsure_parameter = company_details[5].css(
-                    '.text-md::text').getall()
-                unsure_parameter_key = unsure_parameter[0].lower().strip()
-                company_item[unsure_parameter_key] = unsure_parameter[1].strip()
-                # `founded` comes before specialties if exists, or else `specialties` at first means that `founded` parameter isn't defined
-                if unsure_parameter_key == 'founded':
-                    company_specialties = company_details[6].css(
-                        '.text-md::text').getall()
-                    # after `founded` is extracted, check if `specialties` is defined
-                    if company_specialties[0].lower().strip() == 'specialties':
-                        company_item['specialties'] = company_specialties[1].strip()
+                try:
+                    company_item['website'] = company_details[0].css('a::text').get(default='not-found').strip()
+                except:
+                    company_item['website'] = ""
+                try:
+                    company_industry_line = company_details[1].css('.text-md::text').getall()
+                    company_item['industry'] = company_industry_line[1].strip()
+                except:
+                    company_item['industry'] = ""
+                try:
+                    company_size_line = company_details[2].css('.text-md::text').getall()
+                    company_item['company_size_approx'] = company_size_line[1].strip().split()[0]
+                except:
+                    company_item['company_size_approx'] = ""
+                try:
+                    company_headquarters = company_details[3].css('.text-md::text').getall()
+                    if company_headquarters[0].lower().strip() == 'headquarters':
+                        company_item['headquarters'] = company_headquarters[1].strip()
                     else:
+                        company_item['headquarters'] = 'not-found'
+                except:
+                    company_item['headquarters'] = ""
+                try:
+                    company_type = company_details[4].css('.text-md::text').getall()
+                    company_item['type'] = company_type[1].strip()
+                except:
+                    company_item['type'] = ""
+                try:
+                    # specialities or founded, one among them -> storing in `unsure_parameter`
+                    unsure_parameter = company_details[5].css('.text-md::text').getall()
+                    unsure_parameter_key = unsure_parameter[0].lower().strip()
+                    company_item[unsure_parameter_key] = unsure_parameter[1].strip()
+                    # `founded` comes before specialties if exists, or else `specialties` at first means that `founded` parameter isn't defined
+                    if unsure_parameter_key == 'founded':
+                        company_specialties = company_details[6].css('.text-md::text').getall()
+                        # after `founded` is extracted, check if `specialties` is defined
+                        if company_specialties[0].lower().strip() == 'specialties':
+                            company_item['specialties'] = company_specialties[1].strip()
+                        else:
+                            company_item['specialties'] = 'not-found'
+                    elif unsure_parameter_key != 'specialties' or unsure_parameter_key == 'founded':
+                        company_item['founded'] = 'not-found'
                         company_item['specialties'] = 'not-found'
-                elif unsure_parameter_key != 'specialties' or unsure_parameter_key == 'founded':
-                    company_item['founded'] = 'not-found'
-                    company_item['specialties'] = 'not-found'
-
-                # funding parameters, more feasible error handling to be implemented, if sir needs to have..
-                if False :
-                    company_item['funding'] = response.css(
-                        'p.text-display-lg::text').get(default='not-found').strip()
-                    company_item['funding_total_rounds'] = int(response.xpath(
-                        '//section[contains(@class, "aside-section-container")]/div/a[contains(@class, "link-styled")]//span[contains(@class, "before:middot")]/text()').get(
-                        'not-found').strip().split()[0])
-                    company_item['funding_option'] = response.xpath(
-                        '//section[contains(@class, "aside-section-container")]/div//div[contains(@class, "my-2")]/a[contains(@class, "link-styled")]/text()').get(
-                        'not-found').strip()
-                    company_item['last_funding_round'] = response.xpath(
-                        '//section[contains(@class, "aside-section-container")]/div//div[contains(@class, "my-2")]/a[contains(@class, "link-styled")]//time[contains(@class, "before:middot")]/text()').get(
-                        'not-found').strip()
+                except:
+                    company_item['founded'] = ""
+                    company_item['specialties'] = ""
 
             except IndexError:
                 print("Error: *****Skipped index, as some details are missing*********")
